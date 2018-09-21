9614c1eed7d14bd2fc3152722940313c707a9ad6
Daniil <pohachevskiy.da@phystech.edu>
2018-09-21 12:06:56 +0300

    Added debug logging

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -12,8 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println(string: "\(#function)")
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        println(string: "\(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        println(string: "\(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        println(string: "\(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        println(string: "\(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        println(string: "\(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        println(string: "\(#function)")
+    }
+    
 
 
 }
