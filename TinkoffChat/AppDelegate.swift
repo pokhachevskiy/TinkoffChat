2934fa8ba442145cbabd5ca710d9af6e847624aa
Daniil <pohachevskiy.da@phystech.edu>
2018-09-21 13:56:45 +0300

    fixed applicationState print with UIApplication.State

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -16,39 +16,61 @@ func println(string : String) {
     #endif
 }
 
-
+func parseUIApplicationState (applicationState: UIApplication.State) -> String {
+    switch applicationState {
+        case .active:
+            return "active state"
+        case .background:
+            return "background state"
+        case .inactive:
+            return "inactive state"
+    }
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var currentApplicationState: UIApplication.State = UIApplication.shared.applicationState
+    var previousApplicationState: UIApplication.State = UIApplication.shared.applicationState
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        println(string : "Application moved from not running state to inactive state: \(#function)")
+        currentApplicationState = UIApplication.shared.applicationState
+        println(string : "Application moved from \(parseUIApplicationState(applicationState: previousApplicationState)) to \(parseUIApplicationState(applicationState: currentApplicationState)): \(#function)")
+        previousApplicationState = currentApplicationState
         // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        println(string : "Application moved from active state to inactive state: \(#function)")
+        currentApplicationState = UIApplication.shared.applicationState
+        println(string : "Application moved from \(parseUIApplicationState(applicationState: previousApplicationState)) to \(parseUIApplicationState(applicationState: currentApplicationState)): \(#function)")
+        previousApplicationState = currentApplicationState
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        println(string : "Application moved from inactive state to background state: \(#function)")
+        currentApplicationState = UIApplication.shared.applicationState
+        println(string : "Application moved from \(parseUIApplicationState(applicationState: previousApplicationState)) to \(parseUIApplicationState(applicationState: currentApplicationState)): \(#function)")
+        previousApplicationState = currentApplicationState
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        println(string : "Application moved from background state to inactive state: \(#function)")
+        currentApplicationState = UIApplication.shared.applicationState
+        println(string : "Application moved from \(parseUIApplicationState(applicationState: previousApplicationState)) to \(parseUIApplicationState(applicationState: currentApplicationState)): \(#function)")
+        previousApplicationState = currentApplicationState
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        println(string : "Application moved from inactive state to active state: \(#function)")
+        currentApplicationState = UIApplication.shared.applicationState
+        println(string : "Application moved from \(parseUIApplicationState(applicationState: previousApplicationState)) to \(parseUIApplicationState(applicationState: currentApplicationState)): \(#function)")
+        previousApplicationState = currentApplicationState
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
